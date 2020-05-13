698c463cde9f2a960d6b2ef1b65041020ccde51a
Rafael M Mudafort <rafmudaf@gmail.com>
2020-05-13 00:28:39 -0500

    Add a thread pool

--- combine_videos/video_combine.py
+++ combine_videos/video_combine.py
@@ -1,15 +1,37 @@
 
 import os
+from multiprocessing import cpu_count
+from multiprocessing import Pool
 from moviepy.editor import (
     VideoFileClip,
-    ImageSequenceClip,
     concatenate_videoclips
 )
 import video_processing
 
 
-def process_clips(clips_in):
+def filter_clips(clips_in):
+    # Throw away clips shorter than 9 seconds
+    # - These tend to be cars or bikes passing by quickly
+    clips_out = list( filter( lambda x: x.duration >= 9, clips_in ) )
     return clips_out
+
+def process_clips(clips_in):
+    clips_out = video_processing.down_sample(clips_in)
+    # clips_out = video_processing.drop_frames(clips_in)
+    return clips_out
+
+def process_chunk(i, filenames):
+    chunk = [VideoFileClip( os.path.join(video_directory, f) ) for f in filenames]
+
+    print("Working on chunk {} of {}".format(i + 1, n_chunks))
+    chunk = process_clips(chunk)
+
+    print("Building clip...")
+    final_clip = concatenate_videoclips(chunk)
+
+    print("Exporting video file...")
+    final_clip.write_videofile(video_directory + "_temp_{}".format(i + 1) + ".mp4")
+    final_clip.write_videofile(os.path.join(video_directory, "temp{}".format(i + 1) + ".mp4"))
 
 
 video_directory = "20200509"
@@ -23,35 +45,30 @@ video_paths = list( filter( lambda x: ".mp4" in x, os.listdir(video_directory) )
 # Sort by last modification date
 video_paths.sort( key=lambda x: os.path.getmtime(os.path.join(video_directory, x) ), reverse=True )
 
-clips = [VideoFileClip( os.path.join(video_directory, video) ) for video in video_paths]
-
-# Throw away clips shorter than 9 seconds
-# - These tend to be cars or bikes passing by quickly
-clips = list( filter( lambda x: x.duration >= 9, clips ) )
-
-chunk_size = 5
-n_chunks = len(clips) // chunk_size
+chunk_size = 2
+n_chunks = len(video_paths) // chunk_size
+chunks = []
 for i in range(n_chunks):
-    print("Working on chunk {} of {}".format(i + 1, n_chunks))
-
-    if i < n_chunks - 1:
-        # Automatically set the step size
-        i0 = i*chunk_size
-        i1 = (i+1)*chunk_size
-        chunk = clips[i0:i1]
-    else:
+    i0 = i*chunk_size
+    if i == n_chunks - 1:
         # On the last step, go all the way to the end of the clip array
-        i0 = i*chunk_size
-        chunk = clips[i0:]
+        chunks.append(video_paths[i0:])
+    else:
+        # Otherwise, use the constant step size and fill this chunk
+        i1 = (i+1)*chunk_size
+        chunks.append(video_paths[i0:i1])
 
-    postprocessed = process_clips(chunk)
+arguments = list(
+    zip(
+        [i for i in range(n_chunks)],
+        chunks
+    )
+)
+with Pool(2) as pool:
+    pool.starmap(process_chunk, arguments)
 
-    print("Building clip...")
-    final_clip = concatenate_videoclips(postprocessed)
-
-    print("Exporting video file...")
-    final_clip.write_videofile(video_directory + "_temp_{}".format(i) + ".mp4")
-
-    print("Finished chunk {}".format(i + 1))
-
-
+temp_clips = [
+    VideoFileClip(os.path.join(video_directory, "temp{}".format(i + 1) + ".mp4")) for i in range(n_chunks)
+]
+final = concatenate_videoclips(temp_clips)
+final.write_videofile(os.path.join(video_directory, video_directory + ".raf.mp4"))
